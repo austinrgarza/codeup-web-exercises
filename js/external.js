641c77d1d68ddb9172b2171c1b4578cb6e0edc2d
Austin Garza <Austin.r.garza@gmail.com>
2018-11-25 14:48:11 -0600

    updating assignment list

---
+++ js/external.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// ... code
+
+let myAge = 25;
+
+// .. rest of code
+
+console.log("Hello from external JavaScript!");
+
+alert("Welcome to my website!");
+
+let userFavColor = prompt("What's your favorite color?");
+console.log("My favorite color is " + userFavColor + " too!");
+
+
+
