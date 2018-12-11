72f345633c32d810cd063e1550ae01b743cd6c27
Austin Garza <Austin.r.garza@gmail.com>
2018-12-11 12:29:44 -0600

    fixed typo in gitignore file and adding jquery mouse click exercise

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,26 +1,26 @@
-alert('DOM has finished loading');
-
-$("p").click(function(){
-    alert("The paragraph was clicked.");
-});
-
-
-var contents = $('#main-title').html();
-alert(contents);
-
-
-$("p").css({"border-color": "blue",
-    "border-width":"1px",
-    "border-style":"solid"});
-
-$('#firstParagraph').css({'font-size': '14px'
-
-});
-
+// alert('DOM has finished loading');
+//
+// $("p").click(function(){
+//     alert("The paragraph was clicked.");
+// });
+//
+//
+// var contents = $('#main-title').html();
+// alert(contents);
+//
+//
+// $("p").css({"border-color": "blue",
+//     "border-width":"1px",
+//     "border-style":"solid"});
+//
+// $('#firstParagraph').css({'font-size': '14px'
+//
+// });
+//
 $( function() {
     $( "#draggable" ).draggable();
 } );
-
+//
 $('#draggable').css({
     "border-color":"blue",
     "border-width": "1px",
@@ -31,7 +31,26 @@ $('#draggable').css({
     "text-align": "center",
     "margin-bottom": "0"
 });
+//
+// $("button").click(function(){
+//     $("p").fadeIn();
+// });
 
-$("button").click(function(){
-    $("p").fadeIn();
+//================Mouse events exercise=====================
+
+
+$('h1').click(function () {
+    $('h1').css('background-color', 'orange');
 });
+
+$('p').dblclick(function () {
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(function () {
+    $(this).css('color','red');
+    },
+    function () {
+        $(this).css('color','');
+    }
+);
